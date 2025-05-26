2a836922ecd359300ee9e3737de1d9080719acb6
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-05-26 11:29:54 +0200

    chore: Fix indentation in shell script

--- build/render-loop.bash
+++ build/render-loop.bash
@@ -5,13 +5,13 @@ root="$(realpath "${dir}/..")"
 
 echo "watching ${root} for changes..."
 while true; do
-	changed="$(inotifywait -e modify,create,delete,move --quiet --recursive --include "\.go$" --format "%w%f" "${root}")"
+  changed="$(inotifywait -e modify,create,delete,move --quiet --recursive --include "\.go$" --format "%w%f" "${root}")"
   changedRelativePath="$(realpath -m --relative-to="${root}" "${changed}")"
 
-	echo "change in ./${changedRelativePath}, rebuilding..."
-	if ! devbox run render; then
-		echo "failed to rebuild."
-	else
-	  echo "rebuilt."
-	fi
+  echo "change in ./${changedRelativePath}, rebuilding..."
+  if ! devbox run render; then
+    echo "failed to rebuild."
+  else
+    echo "rebuilt."
+  fi
 done
