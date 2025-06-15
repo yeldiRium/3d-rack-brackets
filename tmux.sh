b4610763a951d7d89c8ea0d989aa6168d90ebfbc
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-06-15 17:52:32 +0200

    chore: Improve tmux script

--- tmux.sh
+++ tmux.sh
@@ -3,38 +3,36 @@ set -eu
 
 session="ide-3d-rack-brackets"
 attach() {
-  if [ -n "${TMUX:-}" ]; then
-    tmux switch-client -t "=${session}"
-  else
-    tmux attach-session -t "=${session}"
-  fi
+	if [ -n "${TMUX:-}" ]; then
+		tmux switch-client -t "=${session}"
+	else
+		tmux attach-session -t "=${session}"
+	fi
 }
 
 if ! tmux has-session -t "${session}" 2>/dev/null; then
-  tmux new-session -d -s "${session}" -x "$(tput cols)" -y "$(tput lines)"
+	tmux new-session -d -s "${session}" -x "$(tput cols)" -y "$(tput lines)"
 
-  tmux split-window -t "${session}:0.0" -h
-  tmux resize-pane -t "${session}:0.0" -x "70%"
+	tmux split-window -t "${session}:0.0" -h -l "70%" -b
 
-	tmux split-window -t "${session}:0.1" -v
-	tmux split-window -t "${session}:0.2" -v
+	tmux split-window -t "${session}:0.1" -v -l "20%" -b
+	tmux split-window -t "${session}:0.2" -v -l "10%" -b
+	tmux split-window -t "${session}:0.3" -v -l "30%" -b
 
-	tmux resize-pane -t "${session}:0.1" -y "10%"
-	tmux resize-pane -t "${session}:0.2" -y "40%"
-	tmux resize-pane -t "${session}:0.3" -y "50%"
+	sleep 1
 
-  sleep 1
+	tmux send-keys -t "${session}:0.0" " devbox shell" "C-m" "C-l"
+	tmux send-keys -t "${session}:0.1" " devbox shell" "C-m" "C-l"
+	tmux send-keys -t "${session}:0.2" " devbox shell" "C-m" "C-l"
+	tmux send-keys -t "${session}:0.3" " devbox shell" "C-m" "C-l"
+	tmux send-keys -t "${session}:0.4" " devbox shell" "C-m" "C-l"
 
-  tmux send-keys -t "${session}:0.0" "devbox shell" "C-m" "C-l"
-  tmux send-keys -t "${session}:0.1" "devbox shell" "C-m" "C-l"
-  tmux send-keys -t "${session}:0.2" "devbox shell" "C-m" "C-l"
-  tmux send-keys -t "${session}:0.3" "devbox shell" "C-m" "C-l"
+	tmux send-keys -t "${session}:0.0" " nvim" "C-m" "M-1"
+	tmux send-keys -t "${session}:0.1" " git bug termui" "C-m"
+	tmux send-keys -t "${session}:0.2" " devbox run open" "C-m"
+	tmux send-keys -t "${session}:0.3" " devbox run watch" "C-m"
 
-  tmux send-keys -t "${session}:0.0" "nvim" "C-m" "M-1"
-	tmux send-keys -t "${session}:0.1" "devbox run open" "C-m"
-	tmux send-keys -t "${session}:0.2" "devbox run watch" "C-m"
-
-  tmux select-pane -t "${session}:0.0"
+	tmux select-pane -t "${session}:0.0"
 fi
 
 attach
