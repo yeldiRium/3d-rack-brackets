017a070af1a6538f65f208c092e75129dfd9bfd0
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-09-21 19:35:24 +0200

    chore: Add cli flag and devbox script for profiling

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
+	"runtime/pprof"
 
 	"github.com/alecthomas/kong"
 
@@ -11,7 +13,8 @@ import (
 )
 
 var cli struct {
-	Debug bool `help:"Enable debug mode."`
+	Debug      bool   `help:"Enable debug mode."`
+	CPUProfile string `type:"path"`
 
 	Render render.RenderCmd `cmd:"" help:"render the rack"`
 }
@@ -24,11 +27,17 @@ func main() {
 	logLevel := slog.LevelInfo
 	if cli.Debug {
 		logLevel = slog.LevelDebug
-	}		
+	}
 	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 		Level: logLevel,
 	})
 	logger := slog.New(handler)
+
+	if err := initializeProfiling(logger); err != nil {
+		logger.Error(err.Error())
+		ctx.Exit(1)
+	}
+	defer stopProfiling()
 
 	err := ctx.Run(&globals.Globals{
 		Debug:  cli.Debug,
@@ -36,4 +45,21 @@ func main() {
 		Stdout: os.Stdout,
 	})
 	ctx.FatalIfErrorf(err)
+}
+
+func initializeProfiling(logger *slog.Logger) error {
+	logger.Debug("cpu profiling", slog.Bool("enabled", cli.CPUProfile != ""), slog.String("outPath", cli.CPUProfile))
+	if cli.CPUProfile != "" {
+		file, err := os.Create(cli.CPUProfile)
+		if err != nil {
+			return fmt.Errorf("failed to open output file for profiling: %w", err)
+		}
+		if err := pprof.StartCPUProfile(file); err != nil {
+			return fmt.Errorf("failed to initialize profiling", err)
+		}
+	}
+	return nil
+}
+func stopProfiling() {
+	pprof.StopCPUProfile()
 }
